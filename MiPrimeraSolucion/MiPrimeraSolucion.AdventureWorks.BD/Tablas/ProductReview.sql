84060b358d8cd60dc880c7503686aaf45a36675d
hecferme <hecferme@gmail.com>
2018-03-08 20:17:26 -0600

    se agrega la tabla de ProductReview al proyecto de BD.

---
+++ MiPrimeraSolucion/MiPrimeraSolucion.AdventureWorks.BD/Tablas/ProductReview.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ProductReview]
+(
+	[ProductReviewID] INT NOT NULL PRIMARY KEY, 
+    [ProductID] INT NULL, 
+    [ReviewerName] NVARCHAR(50) NULL, 
+    [ReviewDate] DATETIME NULL DEFAULT GETDATE(), 
+    [EmailAddress] NVARCHAR(50) NULL, 
+    [Rating] INT NULL, 
+    [Comments] NVARCHAR(3850) NULL, 
+    [ModifiedDate] DATETIME NULL DEFAULT GETDATE(), 
+    CONSTRAINT [FK_ProductReview_Product_ProductID] FOREIGN KEY ([ProductID]) REFERENCES [Product] ([ProductID])
+)
